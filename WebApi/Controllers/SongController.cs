2200b71b215911e49fd638a9bd540a43c480a744
Shubham Dixit <shubham.dixit@external.atlascopco.com>
2022-04-22 11:01:53 +0530

    first commit

---
+++ WebApi/Controllers/SongController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApi.models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongController : ControllerBase
+    {
+        private static List<Song> songs = new List<Song>()
+        {
+            new Song() { Id = 1, Title = "title 1", Language = "English 1" },
+            new Song() { Id = 1, Title = "title 2", Language = "English 2" },
+            new Song() { Id = 1, Title = "title 3", Language = "English 3" }
+        };
+
+        [HttpGet]
+        public IEnumerable<Song> Get()
+        {
+            return songs;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Song song)
+        {
+            songs.Add(song);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Song song)
+        {
+            songs[id] = song;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            songs.RemoveAt(id);
+        }
+    }
+}
